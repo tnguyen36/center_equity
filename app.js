a1bc74bb26b9b596869cc7123bc7b8fcb3f63cc3
Thi Nguyen <tsokye346@gmail.com>
2019-05-08 21:35:19 -0700

    Add password reset feature

--- app.js
+++ app.js
@@ -5,27 +5,40 @@ var bodyParser = require("body-parser");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
 var User = require("./models/user");
+var async = require("async");
+var nodemailer = require("nodemailer");
+var crypto = require("crypto");
+var environmentVariables = require("dotenv").config();
+var { google } = require("googleapis");
+var OAuth2 = google.auth.OAuth2;
 
+
+
+// Connect to database
 const databaseUri = process.env.MONGODB_URI || "mongodb://localhost:27017/center_equity";
 mongoose.connect(databaseUri, {useNewUrlParser: true})
 	.then(() => console.log("Database connected"))
 	.catch(err => console.log("Database connection error: " + err.message));
 
+// App settings
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 
+// Session for storing data
 app.use(require("express-session")( {
 	secret: "Rusty Wins",
 	resave: false,
 	saveUninitialized: false
 }));
 
+// Initialize passport for user login
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 // Root Route
 app.get("/", function(req, res) {
@@ -66,6 +79,127 @@ app.post("/login", passport.authenticate("local",
 
 });
 
-app.listen(process.env.PORT, process.env.IP, function() {
+// Forgot Password Route
+app.get("/forgot", function(req, res) {
+	res.render("forgot");
+});
+
+app.post("/forgot", function(req, res) {
+	async.waterfall([
+		function(done) {
+			crypto.randomBytes(20, function(err, buf) {
+		        var token = buf.toString('hex');
+		        done(err, token);
+      });
+    },
+	    function(token, done) {
+	    	User.findOne({ username: req.body.email}, function(err, user) {
+					if(!user) {
+						return res.redirect("/forgot");
+					}
+					user.resetPasswordToken = token;
+					user.resetPasswordExpires = Date.now() + 3600000;
+
+					user.save(function(err) {
+						done(err, token, user);
+					});
+			});
+    },
+		function(token, user, done) {
+			var oauth2Client = new OAuth2(
+				process.env.CLIENT_ID,
+				process.env.CLIENT_SECRET, // Client Secret
+				process.env.REDIRECT_URL // Redirect URL
+			);
+			var smtpTransport = nodemailer.createTransport({
+				service: "gmail",
+				auth: {
+					type: "OAuth2",
+					user: process.env.MAIL_ACCOUNT,
+					clientId: process.env.CLIENT_ID,
+					clientSecret: process.env.CLIENT_SECRET,
+					refreshToken: process.env.REFRESH_TOKEN,
+					accessToken:  process.env.ACCESS_TOKEN,
+					expires: 1484314697598
+				}
+			});
+			var mailOptions = {
+				to: user.username,
+				from: "Center Equity <" + process.env.MAIL_ACCOUNT + ">",
+				subject: "Password Reset",
+				text: "Please click on the following link below, or paste this into your browser to complete the process:\n\n" +
+					"http://" + req.headers.host + "/reset/" + token +  "\n\n" +
+					"If you did not request this, please ignore this email and your password will remain unchanged"
+			};
+			smtpTransport.sendMail(mailOptions, function(err) {
+				console.log("mail sent");
+				done(err, "done");
+			});
+
+		}
+	], function(err) {
+		if (err) return next(err);
+		res.redirect("/forgot");
+	});
+});
+				
+
+
+// Reset Email Link Route
+app.get("/reset/:token", function(req, res) {	
+	res.render("reset");
+
+});
+
+app.post('/reset', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          return res.redirect('back');
+        }
+        if(req.body.password === req.body.confirm) {
+          user.setPassword(req.body.password, function(err) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpires = undefined;
+
+            user.save(function(err) {
+              req.logIn(user, function(err) {
+                done(err, user);
+              });
+            });
+          })
+        } else {
+            return res.redirect('back');
+        }
+      });
+    }
+    // function(user, done) {
+    //   var smtpTransport = nodemailer.createTransport({
+    //     service: 'Gmail', 
+    //     auth: {
+    //       user: process.env.MAIL_ACCOUNT,
+    //       pass: process.env.MAIL_PW
+    //     }
+    //   });
+    //   var mailOptions = {
+    //     to: user.username,
+    //     from: process.env.MAIL_ACCOUNT,
+    //     subject: 'Your password has been changed',
+    //     text: 'Hello,\n\n' +
+    //       'This is a confirmation that the password for your account ' + user.username + ' has just been changed.\n'
+    //   };
+    //   smtpTransport.sendMail(mailOptions, function(err) {
+    //     done(err);
+    //   });
+    // }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect("/login");
+  });
+});
+
+// http request listener
+app.listen(process.env.port, process.env.ip, function() {
 	console.log("Server has started!");
 });
--- models/user.js
+++ models/user.js
@@ -4,11 +4,13 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
-	username: String,
+	username: {type: String, unique: true},
 	rank: String,
 	reason: String,
 	password: String,
-	subscribe: String
+	subscribe: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
